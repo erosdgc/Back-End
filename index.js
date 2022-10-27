eb7781433f7fda26f20df30bf73c2498c702fdc0
Eros D <gomezeros@gmail.com>
2022-10-26 23:54:54 -0300

    Fix

--- index.js
+++ index.js
@@ -1,8 +1,34 @@
-class  usuario {
-    constructor (nombre, apellido, libros, mascotas) {
-        this.nombre = nombre,
-        this.apellido = apellido,
-        this.libros = libros,
-        this.mascotas = mascotas
+class User {
+    constructor(firstName, lastName, books, pets) {
+        this.firstName = firstName,
+            this.lastName = lastName,
+            this.books = books,
+            this.pets = pets
+    }
+    getFullName() {
+        return (`${this.firstName} ${this.lastName}`)
+    }
+    addBook(bookName, author) {
+        return (this.books.push({ bookName, author }))
+    }
+    getBooksNames() {
+        const mapBookNames = this.books.map(book => book.bookName)
+        console.log(`${this.getFullName()} has these books: ${mapBookNames}`)
+    }
+    addMascota(namePet) {
+        return (this.pets.push(namePet))
+    }
+    countMascotas() {
+        return (`${(this.firstName)} lives with ${this.pets.length} pets`)
     }
 }
+
+const test = new User("Eros", "David", [], [])
+
+console.log(test.getFullName())
+test.addMascota("Zazu"); void
+test.addMascota("Wilson"); void
+console.log(test.countMascotas([]))
+test.addBook("El señor de las moscas", "William Golding"); void
+test.addBook("Fundación", "Isaac Asimov"); void
+test.getBooksNames([])
