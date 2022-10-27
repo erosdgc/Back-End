c2f5fc52b1174c39239b51352bbafc842edb8d06
Eros D <gomezeros@gmail.com>
2022-10-26 23:29:07 -0300

    Commit 1

---
+++ index.js
@@ -0,0 +1,8 @@
+class  usuario {
+    constructor (nombre, apellido, libros, mascotas) {
+        this.nombre = nombre,
+        this.apellido = apellido,
+        this.libros = libros,
+        this.mascotas = mascotas
+    }
+}
