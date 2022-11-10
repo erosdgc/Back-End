5875c1c20f0ad835a6e77bfbd372d0a2a5906e48
Eros D <gomezeros@gmail.com>
2022-11-09 23:15:10 -0300

    express-server-commit-1

--- server.js
+++
@@ -1,9 +0,0 @@
-const http = require('http')
-
-const server = http.createServer((message, response) => {
-    response.end('Hola Eros')
-})
-
-const connection = server.listen(8080, () => {
-    console.log('Servidor escuchando en el 8080')
-})
